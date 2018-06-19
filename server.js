27ccf0ec259e2fe12640a50d3ba3a8aa694937e4
ankitbaheti <ankit.baheti04@gmail.com>
2018-06-19 00:36:39 -0400

    added cross origin tag

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin",
-    "*");
+    "https://webdev-summer1-angular-client.herokuapp.com");
   res.header("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods",
